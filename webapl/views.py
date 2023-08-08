d2ac5eaab90fee9f094d58d11ef6de7634096c69
nasu0720 <lnaxutian@gmail.com>
2023-08-08 16:33:16 +0900

    ページを移動せずに結果が見られるようにしました

--- webapl/views.py
+++ webapl/views.py
@@ -1,29 +1,36 @@
 from flask import Flask
-from flask import render_template, request
+from webapl import app
+from flask import render_template, request, flash, redirect, url_for
 import re
-app = Flask(__name__)
 
 @app.route("/")
 def landing_page():
     return render_template('lp.html')
+
 # 素数判定
-@app.route("/result", methods=["POST"])
+@app.route("/", methods=["POST"])
 def is_prime(n=0):
+    result="素数です"
     data=request.form
     n=data.get("number")
-    result="素数です"
     match=re.fullmatch(r'^[1-9][0-9]*$',str(n))
     if match==None:
         result="自然数を入力してください"
+        
+        
     
     else:
         n=int(n)    
         if n < 2:
             result="素数ではありません"
-
+            
+            
         else:
             for i in range(2, int(n**0.5) + 1):
                 if n % i == 0:
                     result="素数ではありません"
-
-    return render_template('result.html', result=result)
+                    flash("素数ではありません")
+                else:
+                    result="素数です"
+                    
+    return render_template("lp.html", result=result, num=n)   
